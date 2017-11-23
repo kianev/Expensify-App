8238a885aac31f5dd3e7d3a3869345cab816a7f0
Kaloian Ianev <kianev@gmail.com>
2017-11-23 22:19:16 +0200

    LoadingPage, Loader added.

--- expensify/src/index.js
+++ expensify/src/index.js
@@ -10,6 +10,7 @@ import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
+import LoadingPage from './components/LoadingPage';
 
 
 const store = configureStore();
@@ -28,7 +29,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('root'))
+ReactDOM.render(<LoadingPage />, document.getElementById('root'))
 
 firebase.auth().onAuthStateChanged((user) => {
   if(user){
